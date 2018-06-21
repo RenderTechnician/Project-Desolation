6fe4d713645c6809917b324f8319f5169f7ebdaf
Will Rathlou <willrathlou@hotmail.com>
2018-06-21 01:56:45 +0100

    21.06.2018

---
+++ Five Nights At Blue Thing Remastered/Assets/Scripts/Loading/Loading.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour {
+    public int currentnight;
+    public Text whichnight;
+    public int time;
+	// Use this for initialization
+	void Start () {
+        currentnight = PlayerPrefs.GetInt("Currentnight");
+        whichnight.text = "Night " + (currentnight + 1);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time++;
+        if(time > 600) { SceneManager.LoadScene("night 1-5"); }
+    }
+}
